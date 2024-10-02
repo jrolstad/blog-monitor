6a45cb066d43adc7edcbd1e572f4433be11c0cea
Josh Rolstad <jrolstad@gmail.com>
2024-10-01 20:42:10 -0700

    Adding Seattle Area Forecast Discussion (#6)
    * Adding existing
    
    * Adding existing
    
    * Adding seattle area forecast discussion

--- deployments/infra/data.tf
+++ deployments/infra/data.tf
@@ -23,6 +23,75 @@ resource "aws_dynamodb_table_item" "subscription_cliffmassweather" {
   },
   "url": {
     "S": "https://cliffmass.blogspot.com"
+  },
+  "type": {
+      "S": "blogger"
+  }
+}
+ITEM
+}
+
+resource "aws_dynamodb_table_item" "subscription_wasmoke" {
+  table_name = aws_dynamodb_table.subscriptions.name
+  hash_key   = aws_dynamodb_table.subscriptions.hash_key
+
+  item = <<ITEM
+{
+  "id": {
+    "S": "2"
+  },
+  "maximumLookback": {
+    "N": "5"
+  },
+  "name": {
+    "S": "Washington Smoke information"
+  },
+  "notificationMethod": {
+    "S": "email"
+  },
+  "notificationTargets": {
+    "SS": [
+      "jrolstad@gmail.com"
+    ]
+  },
+  "url": {
+    "S": "https://wasmoke.blogspot.com/"
+  },
+  "type": {
+      "S": "blogger"
+  }
+}
+ITEM
+}
+
+resource "aws_dynamodb_table_item" "subscription_nws_seattle_area_forecast" {
+  table_name = aws_dynamodb_table.subscriptions.name
+  hash_key   = aws_dynamodb_table.subscriptions.hash_key
+
+  item = <<ITEM
+{
+  "id": {
+    "S": "3"
+  },
+  "maximumLookback": {
+    "N": "5"
+  },
+  "name": {
+    "S": "Seattle Area Forecast Discussion"
+  },
+  "notificationMethod": {
+    "S": "email"
+  },
+  "notificationTargets": {
+    "SS": [
+      "jrolstad@gmail.com"
+    ]
+  },
+  "url": {
+    "S": "https://api.weather.gov/products/types/AFD/locations/SEW"
+  },
+  "type": {
+      "S": "national-weather-service"
   }
 }
 ITEM
