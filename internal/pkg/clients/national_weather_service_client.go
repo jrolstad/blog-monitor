87055e2be4610523addd9c97eb50c3b03245cf05
Josh Rolstad <jrolstad@gmail.com>
2024-10-01 21:34:40 -0700

    Enabling sending of most recent post

---
+++ internal/pkg/clients/national_weather_service_client.go
@@ -0,0 +1,101 @@
+package clients
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+type NationalWeatherServiceClient struct {
+}
+
+func NewNationalWeatherServiceClient() (*NationalWeatherServiceClient, error) {
+	return &NationalWeatherServiceClient{}, nil
+}
+
+func (s *NationalWeatherServiceClient) GetData(url string) ([]NWSProduct, error) {
+	data, err := s.fetchAPIResponse(url)
+	if err != nil {
+		return []NWSProduct{}, err
+	}
+	return data.Graph, nil
+}
+
+func (s *NationalWeatherServiceClient) GetItem(url string) (NWSProduct, error) {
+	data, err := s.fetchAPIResponseSingleItem(url)
+	if err != nil {
+		return NWSProduct{}, err
+	}
+	return data, nil
+}
+
+func (s *NationalWeatherServiceClient) fetchAPIResponse(apiURL string) (NWSApiResponse, error) {
+	var response NWSApiResponse
+
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		return response, fmt.Errorf("error making GET request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return response, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return response, fmt.Errorf("error reading response body: %v", err)
+	}
+
+	// Unmarshal the JSON response into the Response struct
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return response, fmt.Errorf("error unmarshalling JSON: %v", err)
+	}
+
+	return response, nil
+}
+
+func (s *NationalWeatherServiceClient) fetchAPIResponseSingleItem(apiURL string) (NWSProduct, error) {
+	var response NWSProduct
+
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		return response, fmt.Errorf("error making GET request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return response, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return response, fmt.Errorf("error reading response body: %v", err)
+	}
+
+	// Unmarshal the JSON response into the Response struct
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return response, fmt.Errorf("error unmarshalling JSON: %v", err)
+	}
+
+	return response, nil
+}
+
+type NWSProduct struct {
+	ID              string    `json:"id"`
+	URL             string    `json:"@id"`
+	WmoCollectiveID string    `json:"wmoCollectiveId"`
+	IssuingOffice   string    `json:"issuingOffice"`
+	IssuanceTime    time.Time `json:"issuanceTime"`
+	ProductCode     string    `json:"productCode"`
+	ProductName     string    `json:"productName"`
+	ProductText     string    `json:"productText"`
+}
+
+type NWSApiResponse struct {
+	Graph []NWSProduct `json:"@graph"`
+}
--- internal/pkg/processors/nws_subscription_processor.go
+++ internal/pkg/processors/nws_subscription_processor.go
@@ -1,6 +1,8 @@
 package processors
 
 import (
+	"fmt"
+	"github.com/jrolstad/blog-monitor/internal/pkg/clients"
 	"github.com/jrolstad/blog-monitor/internal/pkg/logging"
 	"github.com/jrolstad/blog-monitor/internal/pkg/models"
 	"github.com/jrolstad/blog-monitor/internal/pkg/repositories"
@@ -8,6 +10,7 @@ import (
 )
 
 type NationalWeatherServiceSubscriptionProcessor struct {
+	client *clients.NationalWeatherServiceClient
 }
 
 func (s *NationalWeatherServiceSubscriptionProcessor) ProcessSubscription(notificationHistoryRepository repositories.NotificationHistoryRepository,
@@ -16,5 +19,54 @@ func (s *NationalWeatherServiceSubscriptionProcessor) ProcessSubscription(notifi
 
 	logging.LogEvent("NationalWeatherServiceSubscriptionProcessor.ProcessSubscription", "subscription", subscription.Id)
 
+	posts, err := s.client.GetData(subscription.BlogUrl)
+	logging.LogDependency("NationalWeatherServiceClient", "action", "GetData", "success", err == nil)
+	if err != nil {
+		return err
+	}
+
+	mostRecentItem, err := getMostRecentItem(posts)
+	if err != nil {
+		return err
+	}
+	logging.LogEvent("GetMostRecentItem", "IssuedAt", mostRecentItem.IssuanceTime.Format("2006-01-02 15:04:05 MST"))
+
+	alreadyNotified, err := notificationHistoryRepository.Exists(subscription.Id, mostRecentItem.URL)
+	if err != nil {
+		return err
+	}
+	if alreadyNotified {
+		return nil
+	}
+
+	item, err := s.client.GetItem(mostRecentItem.URL)
+	logging.LogDependency("NationalWeatherServiceClient", "action", "GetItem", "success", err == nil)
+	if err != nil {
+		return err
+	}
+
+	title := fmt.Sprintf("[%s] : %s", subscription.Name, item.ProductName)
+	content := fmt.Sprintf("<p>Source: %s<p> %s", item.URL, item.ProductText)
+	err = notificationService.Notify(subscription.NotificationMethod, subscription.NotificationTargets, title, content)
+	if err != nil {
+		return err
+	}
+	logging.LogEvent("ProcessSubscriptionNotified", "subscription", subscription.Id, "post", item.URL)
+
 	return nil
+}
+
+func getMostRecentItem(items []clients.NWSProduct) (clients.NWSProduct, error) {
+	if len(items) == 0 {
+		return clients.NWSProduct{}, fmt.Errorf("no products available")
+	}
+
+	mostRecent := items[0]
+	for _, product := range items[1:] {
+		if product.IssuanceTime.After(mostRecent.IssuanceTime) {
+			mostRecent = product
+		}
+	}
+
+	return mostRecent, nil
 }
