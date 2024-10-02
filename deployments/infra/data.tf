4c7fc32aaa5904cc2d335ba7607393d2728f5cc7
Josh Rolstad <jrolstad@gmail.com>
2024-10-01 20:26:55 -0700

    Adding seattle area forecast discussion

--- deployments/infra/data.tf
+++ deployments/infra/data.tf
@@ -63,3 +63,36 @@ resource "aws_dynamodb_table_item" "subscription_wasmoke" {
 }
 ITEM
 }
+
+resource "aws_dynamodb_table_item" "subscription_nws_seattle_area_forecast" {
+  table_name = aws_dynamodb_table.subscriptions.name
+  hash_key   = aws_dynamodb_table.subscriptions.hash_key
+
+  item = <<ITEM
+{
+  "id": {
+    "S": "3"
+  },
+  "maximumLookback": {
+    "N": "5"
+  },
+  "name": {
+    "S": "Seattle Area Forecast Discussion"
+  },
+  "notificationMethod": {
+    "S": "email"
+  },
+  "notificationTargets": {
+    "SS": [
+      "jrolstad@gmail.com"
+    ]
+  },
+  "url": {
+    "S": "https://api.weather.gov/products/types/AFD/locations/SEW"
+  },
+  "type": {
+      "S": "national-weather-service"
+  }
+}
+ITEM
+}
